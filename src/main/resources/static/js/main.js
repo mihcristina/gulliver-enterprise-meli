cf7d680d11bb23e82796fb066e753649258f2552
mihcristina <contato@michellicristina.com.br>
2022-06-09 18:55:15 -0300

    add ajustes finais

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -203,7 +203,35 @@
 
 })();
 
-let states = ["São Paulo", "Rio de Janeiro", "Rio Grande do Sul"];
+let states = [
+  "Acre (AC)",
+  "Alagoas (AL)",
+  "Amapá (AP)",
+  "Amazonas (AM)",
+  "Bahia (BA)",
+  "Ceará (CE)",
+  "Distrito Federal (DF)",
+  "Espírito Santo (ES)",
+  "Goiás (GO)",
+  "Maranhão (MA)",
+  "Mato Grosso (MT)",
+  "Mato Grosso do Sul (MS)",
+  "Minas Gerais (MG)",
+  "Pará (PA)",
+  "Paraíba (PB)",
+  "Paraná (PR)",
+  "Pernambuco (PE)",
+  "Piauí (PI)",
+  "Rio de Janeiro (RJ)",
+  "Rio Grande do Norte (RN)",
+  "Rio Grande do Sul (RS)",
+  "Rondônia (RO)",
+  "Roraima (RR)",
+  "Santa Catarina (SC)",
+  "São Paulo (SP)",
+  "Sergipe (SE)",
+  "Tocantins (TO)"
+];
 
 let input = document.getElementById("input");
 
